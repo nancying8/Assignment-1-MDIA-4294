5ccc8fd55f29a9da062ce5a3c47fe74e6773825d
Nancy W <159850766+nancying8@users.noreply.github.com>
2025-02-16 22:59:56 -0800

    new comment

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+// / This will help app.js to read JSON data
 const bodyParser = require('body-parser');
+
 const path = require('path');
 
 // Import routes
